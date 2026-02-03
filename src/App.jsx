3c7aefeb659b7c78336f386a08b9979a1529d919
user <akparaliyevikromjon7@gmail.com>
2026-02-03 14:39:12 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Logo from '../public/LOGO.png'
 import { CiSearch } from "react-icons/ci";
 import { CiHeart } from "react-icons/ci";
 import { CiUser } from "react-icons/ci";
-
+import Header from './components/Header/Header';
 import { SlBasket } from "react-icons/sl";
 import { FaShuffle } from "react-icons/fa6";
 import React from 'react';
