74838ff0e2b7d31ac21eb5ccaec17f6386049ac2
user <akparaliyevikromjon7@gmail.com>
2026-02-03 14:45:45 +0500

    added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({data}) => {
   return (
     <div>
-      
+      <div className="Header">
+        <div className="logo">
+            <img src={data.img} alt="jj" />
+            <p>{data.title}</p>
+        </div>
+        
+      </div>
     </div>
   )
 }
