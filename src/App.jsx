edcd423e340a784397b6b25f54d29aa7fe5c7186
user <akparaliyevikromjon7@gmail.com>
2026-02-03 14:37:33 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import Logo from '../public/LOGO.png'
+import { CiSearch } from "react-icons/ci";
+import { CiHeart } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
 
+import { SlBasket } from "react-icons/sl";
+import { FaShuffle } from "react-icons/fa6";
+import React from 'react';
+const headerData =[
+  {
+    img:Logo,
+    title:'BESTXTORE'
+  },
+  {
+    input:{
+      title:'search',
+      subtitle:'All categories',
+      icon:<CiSearch />
+    }
+  },
+  {
+    icons:[<CiHeart />,<FaShuffle />,<SlBasket />,<CiUser />]
+  }
+]
 const App = () => {
   return (
     <div>
